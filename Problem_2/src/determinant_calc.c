7d779f5452f0ef08d8df03fc6ebbcc2750c03b22
joaocarvalho19 <joao.carvalho19@hotmail.com>
2022-03-21 16:57:27 +0000

    problem 2 done

--- Problem_2/src/determinant_calc.c
+++ Problem_2/src/determinant_calc.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <libgen.h>
 #include <string.h>
-#include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 
 int M;
 int N;
+static void printUsage(char *cmdName);
+#define MAX_STRING_SIZE 20
 
 int gaussianElimination(double mat[N][N]);
 
@@ -100,37 +102,79 @@ void showMatrix(double mat[N][N]){
  */
 int** process_file(FILE *file){
     fread(&M,sizeof(M),1,file);
-    printf("M: %d\n", M);
+    printf("Number of matrices to be read = %d\n", M);
 
     fread(&N,sizeof(N),1,file);
-    printf("N: %d\n", N);
+    printf("Matrices order = %d\n", N);
 
     double coef[M][N][N]; // Matrix Coefficients
     for (int m=0; m<M; m++) {
         fread(&coef[m], sizeof(double[N * N]), 1, file);
 
+        printf("Processing matrix %d ...\n", m+1);
         //showMatrix(coef[m]);
         double determinat = det(coef[m]);
         //printf("\n\n");
         //showMatrix(coef[m]);
-        printf("MATRIX: %d \t Determinat: %f\n", m, determinat);
+        printf("The determinant is %f\n", determinat);
         //printf("\n\n\n\n");
     }
 
     return 0;
 }
 
-
-/** Compute the determinant of the matrix
- *
- * @param c
- * @return int
- */
-int calc_determinant(){
-    return 0;
+static void printUsage(char *cmdName){
+    fprintf(stderr, "\nSynopsis: %s OPTIONS [filename / positive number]\n"
+    " OPTIONS:\n"
+    " -h --- print this help\n"
+    " -f --- filename\n"
+    " -n --- positive number\n", cmdName);
 }
 
+
 int main(int argc, char **argv){
+    int opt; /* selected option */
+    //char *fName = "no name"; /* file name (initialized to "no name" by default) */
+    int val = -1; /* numeric value (initialized to -1 by default) */
+    opterr = 0;
+    char *files_array[MAX_STRING_SIZE];
+    int array_index = 0;
+    do{ 
+        switch ((opt = getopt (argc, argv, "f:n:h"))){ 
+            case 'f': /* file name */
+                if (optarg[0] == '-'){
+                    fprintf(stderr, "%s: file name is missing\n", basename (argv[0]));
+                    printUsage (basename (argv[0]));
+                    return EXIT_FAILURE;
+                }
+                files_array[array_index] = optarg;
+                array_index++;
+                //fName = optarg;
+                break;
+            case 'n': /* numeric argument */
+                if (atoi(optarg) <= 0){
+                    fprintf(stderr, "%s: non positive number\n", basename (argv[0]));
+                    printUsage (basename (argv[0]));
+                    return EXIT_FAILURE;
+                }
+                val = (int) atoi(optarg);
+                break;
+            case 'h': /* help mode */
+                printUsage(basename (argv[0]));
+                return EXIT_SUCCESS;
+            case '?': /* invalid option */
+                fprintf(stderr, "%s: invalid option\n", basename(argv[0]));
+                printUsage (basename (argv[0]));
+                return EXIT_FAILURE;
+            case -1: break;
+        }
+    } while (opt != -1);
+    if (argc == 1){ 
+        fprintf(stderr, "%s: invalid format\n", basename(argv[0]));
+        printUsage (basename (argv[0]));
+        return EXIT_FAILURE;
+    }
+
     // Calculate the time taken
     clock_t t;
     t = clock();
@@ -138,9 +182,9 @@ int main(int argc, char **argv){
     FILE *file;
 
     //for all files in arguments
-    for (int i = 1; i < argc; ++i){
-
-        file = fopen(argv[i],"rb+");  // r for read, b for binary
+    for(int i = 0; i < array_index; ++i){
+        printf("\nFile %d: \n", i+1);
+        file = fopen(files_array[i],"rb+");  // r for read, b for binary
 
         if (file == NULL){
             printf("\nUnable to open file.\n");
@@ -150,9 +194,17 @@ int main(int argc, char **argv){
         fclose(file);
     }
 
+    /*file = fopen(fName,"rb+");  // r for read, b for binary
+    if (file == NULL){
+        printf("\nUnable to open file.\n");
+        exit(EXIT_FAILURE);
+    }
+    process_file(file);
+    fclose(file);*/
+
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC; // calculate the elapsed time
-    printf("Elapsed time: %f seconds\n", time_taken);
+    printf("Elapsed time: %f s\n", time_taken);
 
     return 0;
 }
